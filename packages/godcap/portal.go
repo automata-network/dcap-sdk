689e141960a22e2680bf08e80ab1db389d12bdac
anim001k <140460766+anim001k@users.noreply.github.com>
2025-07-29 14:36:36 +0200

    Update portal.go

--- packages/godcap/portal.go
+++ packages/godcap/portal.go
@@ -243,7 +243,7 @@ func (p *DcapPortal) WaitTx(ctx context.Context, tx *types.Transaction) <-chan *
 					continue
 				}
 
-				logex.Infof("tx receipt %v comfirmed on %v", tx.Hash(), receipt.BlockNumber)
+				logex.Infof("tx receipt %v confirmed on %v", tx.Hash(), receipt.BlockNumber)
 				if p.chain.Explorer != "" {
 					logex.Infof("explorer: %v/tx/%v", p.chain.Explorer, tx.Hash())
 				}
@@ -294,7 +294,7 @@ func (p *DcapPortal) VerifyAndAttestOnChain(opts *bind.TransactOpts, rawQuote []
 // Note: EnableZkProof() should be called before using this function.
 func (p *DcapPortal) GenerateZkProof(ctx context.Context, ty zkdcap.ZkType, quote []byte) (*zkdcap.ZkProof, error) {
 	if p.zkProof == nil {
-		return nil, logex.NewErrorf("DcapPortal should call EnableZkProof() frist")
+		return nil, logex.NewErrorf("DcapPortal should call EnableZkProof() first")
 	}
 	parser := parser.NewQuoteParser(quote)
 	collateral, err := zkdcap.NewCollateralFromQuoteParser(ctx, parser, p.pccs)
