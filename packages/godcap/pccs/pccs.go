6e15c242d398ad61937db68f39b01ae381e67d89
exh0115 <165619875+exh0115@users.noreply.github.com>
2025-12-22 14:06:41 +0800

    Fix TcbEvalDao

--- packages/godcap/pccs/pccs.go
+++ packages/godcap/pccs/pccs.go
@@ -10,6 +10,7 @@ import (
 	"github.com/automata-network/dcap-sdk/packages/godcap/stubs/AutomataEnclaveIdentityDao"
 	"github.com/automata-network/dcap-sdk/packages/godcap/stubs/AutomataFmspcTcbDao"
 	"github.com/automata-network/dcap-sdk/packages/godcap/stubs/AutomataPcsDao"
+	"github.com/automata-network/dcap-sdk/packages/godcap/stubs/AutomataTcbEvalDao"
 	"github.com/chzyer/logex"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -30,19 +31,22 @@ const (
 	ENCLAVE_ID_TDQE
 )
 
+// TcbId constants for TCB type
+const (
+	TCB_ID_SGX uint8 = 0
+	TCB_ID_TDX uint8 = 1
+)
+
 // Client holds the Ethereum client and contract instances
 type Client struct {
-	client  *ethclient.Client
-	network *registry.Network
-	pcs     *AutomataPcsDao.AutomataPcsDao
+	client     *ethclient.Client
+	network    *registry.Network
+	pcs        *AutomataPcsDao.AutomataPcsDao
+	tcbEvalDao *AutomataTcbEvalDao.AutomataTcbEvalDao
 
 	// Versioned DAO instances - lazily initialized
 	fmspcDaos     map[uint32]*AutomataFmspcTcbDao.AutomataFmspcTcbDao
 	enclaveIdDaos map[uint32]*AutomataEnclaveIdentityDao.AutomataEnclaveIdentityDao
-
-	// Default DAO instances (highest version or legacy)
-	defaultFmspc     *AutomataFmspcTcbDao.AutomataFmspcTcbDao
-	defaultEnclaveId *AutomataEnclaveIdentityDao.AutomataEnclaveIdentityDao
 }
 
 // NewClient initializes a new Client instance from a Network
@@ -53,38 +57,31 @@ func NewClient(client *ethclient.Client, network *registry.Network) (*Client, er
 		return nil, logex.Trace(err, network.Contracts.Pccs.PcsDao)
 	}
 
-	// Get default addresses for versioned DAOs
-	fmspcAddr, err := network.GetDefaultFmspcTcbDaoAddress()
-	if err != nil {
-		return nil, logex.Trace(err, "failed to get default FmspcTcbDao address")
-	}
-
-	enclaveIdAddr, err := network.GetDefaultEnclaveIdDaoAddress()
-	if err != nil {
-		return nil, logex.Trace(err, "failed to get default EnclaveIdDao address")
-	}
-
-	// Initialize default FmspcTcbDao
-	defaultFmspc, err := AutomataFmspcTcbDao.NewAutomataFmspcTcbDao(fmspcAddr, client)
-	if err != nil {
-		return nil, logex.Trace(err, fmspcAddr)
-	}
-
-	// Initialize default EnclaveIdentityDao
-	defaultEnclaveId, err := AutomataEnclaveIdentityDao.NewAutomataEnclaveIdentityDao(enclaveIdAddr, client)
-	if err != nil {
-		return nil, logex.Trace(err, enclaveIdAddr)
+	// Initialize TcbEvalDao if available (v1.1 networks)
+	var tcbEvalDao *AutomataTcbEvalDao.AutomataTcbEvalDao
+	if network.Contracts.Pccs.TcbEvalDao != ([20]byte{}) {
+		tcbEvalDao, err = AutomataTcbEvalDao.NewAutomataTcbEvalDao(network.Contracts.Pccs.TcbEvalDao, client)
+		if err != nil {
+			return nil, logex.Trace(err, network.Contracts.Pccs.TcbEvalDao)
+		}
 	}
 
 	return &Client{
-		client:           client,
-		network:          network,
-		pcs:              pcs,
-		fmspcDaos:        make(map[uint32]*AutomataFmspcTcbDao.AutomataFmspcTcbDao),
-		enclaveIdDaos:    make(map[uint32]*AutomataEnclaveIdentityDao.AutomataEnclaveIdentityDao),
-		defaultFmspc:     defaultFmspc,
-		defaultEnclaveId: defaultEnclaveId,
+		client:        client,
+		network:       network,
+		pcs:           pcs,
+		tcbEvalDao:    tcbEvalDao,
+		fmspcDaos:     make(map[uint32]*AutomataFmspcTcbDao.AutomataFmspcTcbDao),
+		enclaveIdDaos: make(map[uint32]*AutomataEnclaveIdentityDao.AutomataEnclaveIdentityDao),
 	}, nil
+}
+
+// GetStandardTcbEvalNum queries the TcbEvalDao to get the standard TCB evaluation number
+func (p *Client) GetStandardTcbEvalNum(ctx context.Context, tcbId uint8) (uint32, error) {
+	if p.tcbEvalDao == nil {
+		return 0, logex.NewError("TcbEvalDao not available (v1.0 network)")
+	}
+	return p.tcbEvalDao.Standard(&bind.CallOpts{Context: ctx}, tcbId)
 }
 
 // GetFmspcTcbDaoForVersion returns the FmspcTcbDao for a specific TCB eval version
@@ -181,14 +178,22 @@ func (p *Client) GetTcbInfo(ctx context.Context, tcbType uint8, fmspc string, tc
 func (p *Client) GetTcbInfoWithEvalNum(ctx context.Context, tcbType uint8, fmspc string, tcbVersion uint32, tcbEvalNum *uint32) (*TcbInfo, error) {
 	var dao *AutomataFmspcTcbDao.AutomataFmspcTcbDao
 	var err error
+	var evalNum uint32
 
 	if tcbEvalNum != nil {
-		dao, err = p.GetFmspcTcbDaoForVersion(*tcbEvalNum)
-		if err != nil {
-			return nil, logex.Trace(err)
-		}
+		evalNum = *tcbEvalNum
 	} else {
-		dao = p.defaultFmspc
+		// Query TcbEvalDao.standard() to get the current standard evaluation number
+		// tcbType maps to tcbId: 0=SGX, 1=TDX
+		evalNum, err = p.GetStandardTcbEvalNum(ctx, tcbType)
+		if err != nil {
+			return nil, logex.Trace(err, "failed to get standard tcb eval num")
+		}
+	}
+
+	dao, err = p.GetFmspcTcbDaoForVersion(evalNum)
+	if err != nil {
+		return nil, logex.Trace(err)
 	}
 
 	result, err := dao.GetTcbInfo(&bind.CallOpts{Context: ctx}, big.NewInt(int64(tcbType)), fmspc, big.NewInt(int64(tcbVersion)))
@@ -225,14 +230,25 @@ func (p *Client) GetEnclaveID(ctx context.Context, enclaveId uint8, version uint
 func (p *Client) GetEnclaveIDWithEvalNum(ctx context.Context, enclaveId uint8, version uint32, tcbEvalNum *uint32) (*EnclaveIdentityInfo, error) {
 	var dao *AutomataEnclaveIdentityDao.AutomataEnclaveIdentityDao
 	var err error
+	var evalNum uint32
 
 	if tcbEvalNum != nil {
-		dao, err = p.GetEnclaveIdDaoForVersion(*tcbEvalNum)
-		if err != nil {
-			return nil, logex.Trace(err)
-		}
+		evalNum = *tcbEvalNum
 	} else {
-		dao = p.defaultEnclaveId
+		// Derive tcbId from enclaveId: QE(0) and QVE(1) are SGX, TDQE(2) is TDX
+		tcbId := TCB_ID_SGX
+		if enclaveId == ENCLAVE_ID_TDQE {
+			tcbId = TCB_ID_TDX
+		}
+		evalNum, err = p.GetStandardTcbEvalNum(ctx, tcbId)
+		if err != nil {
+			return nil, logex.Trace(err, "failed to get standard tcb eval num")
+		}
+	}
+
+	dao, err = p.GetEnclaveIdDaoForVersion(evalNum)
+	if err != nil {
+		return nil, logex.Trace(err)
 	}
 
 	result, err := dao.GetEnclaveIdentity(&bind.CallOpts{Context: ctx}, big.NewInt(int64(enclaveId)), big.NewInt(int64(version)))
